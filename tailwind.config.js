95ee295264d6ebe08b0ee307fd0a0729ea7f0652
christiancvillaruz <42919026+christiancvillaruz@users.noreply.github.com>
2022-07-21 22:41:30 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
